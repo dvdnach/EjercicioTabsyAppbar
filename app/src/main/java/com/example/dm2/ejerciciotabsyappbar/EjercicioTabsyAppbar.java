e0cb6dc5571cc4b919939005dd3883b49fca0144
dvdnach <dvdnach@hotmail.com>
2016-11-09 09:13:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dm2/ejerciciotabsyappbar/EjercicioTabsyAppbar.java
@@ -0,0 +1,13 @@
+package com.example.dm2.ejerciciotabsyappbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EjercicioTabsyAppbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_tabsy_appbar);
+    }
+}
